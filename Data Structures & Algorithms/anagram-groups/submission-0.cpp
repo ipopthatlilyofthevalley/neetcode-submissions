a4c588f4dd4dc225186e37a3553fa7d9c0fec3ad
ipopthatlilyofthevalley <fhosen3fatality@gmail.com>
2026-09-09 02:26:37 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> strsAn;
+        
+        for (const auto& str : strs) {
+            string temp = str;
+            sort(temp.begin(), temp.end());
+            strsAn[temp].push_back(str);
+        }
+        
+        vector<vector<string>> result;
+        result.reserve(strsAn.size());
+        
+        transform(strsAn.begin(), strsAn.end(), back_inserter(result), 
+                  [](auto& pair) { 
+                      return move(pair.second); 
+                  });
+        
+        return result;
+    }
+};
