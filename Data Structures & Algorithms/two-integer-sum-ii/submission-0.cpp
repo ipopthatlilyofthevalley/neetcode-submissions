aaa7af74f66c94961345da756c33f32f6b4b3aa6
ipopthatlilyofthevalley <fhosen3fatality@gmail.com>
2026-09-19 03:09:23 +0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l {};
+        int r { static_cast<int>(numbers.size()) - 1};
+        while (l < r) {
+            if (numbers[l] + numbers[r] > target) {
+                r--;
+            } else if (numbers[l] + numbers[r] == target) {
+                return {l + 1, r + 1};
+            } else {
+                l++;
+            }
+        }
+        return {};
+    }
+};
